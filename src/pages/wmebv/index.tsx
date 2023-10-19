50a7df521333298e93b66e458fcdf0888afca76e
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2023-10-19 17:56:36 +0200

    feat: added some webcomponents to play around

--- src/pages/wmebv/index.tsx
+++ src/pages/wmebv/index.tsx
@@ -1,5 +1,14 @@
 import Layout from '@theme/Layout';
-import { UtrechtHeading1, UtrechtPageContent } from '@utrecht/web-component-library-react';
+import {
+  UtrechtFormFieldDescription,
+  UtrechtHeading1,
+  UtrechtHeading2,
+  UtrechtPageContent,
+  UtrechtParagraph,
+  UtrechtFormFieldTextarea,
+  UtrechtFormFieldTextbox,
+  UtrechtButton,
+} from '@utrecht/web-component-library-react';
 import clsx from 'clsx';
 import React from 'react';
 
@@ -8,7 +17,31 @@ const WmebvPage = () => {
     <Layout>
       <UtrechtPageContent className={clsx('voorbeeld-theme')}>
         <main>
-          <UtrechtHeading1>Voorbeeld</UtrechtHeading1>
+          <UtrechtHeading1>Melding openbare ruimte</UtrechtHeading1>
+          <UtrechtParagraph lead>Vul alle velden in. als een veld optioneel is, staat dit erbij</UtrechtParagraph>
+          <UtrechtHeading2>Wat wilt u melden?</UtrechtHeading2>
+          <UtrechtParagraph>Kies een categorie die bij uw melding past</UtrechtParagraph>
+          <UtrechtFormFieldTextbox></UtrechtFormFieldTextbox>
+          <UtrechtFormFieldTextbox></UtrechtFormFieldTextbox>
+          <UtrechtFormFieldDescription>
+            Beschrijf hier wat er aan de hand is en eventueel wat er aan gedaan kan worden.
+          </UtrechtFormFieldDescription>
+          <UtrechtFormFieldTextarea></UtrechtFormFieldTextarea>
+          <UtrechtButton appearance="secondary-action-button">Bestand kiezen</UtrechtButton>
+          <UtrechtParagraph>(optioneel)</UtrechtParagraph>
+          <UtrechtHeading2>Op welke locatie heeft de melding betrekking? </UtrechtHeading2>
+          <UtrechtParagraph></UtrechtParagraph>
+          <UtrechtFormFieldTextbox></UtrechtFormFieldTextbox>
+          <UtrechtFormFieldTextbox></UtrechtFormFieldTextbox>
+          <UtrechtFormFieldTextarea></UtrechtFormFieldTextarea>
+          <UtrechtHeading2>Hoe kunnen we u bereiken voor meer informatie?</UtrechtHeading2>
+          <UtrechtFormFieldDescription>
+            Als u iets anoniem meld kunnen we u niet informeren over het verloop van de afhandeling voor deze melding.
+          </UtrechtFormFieldDescription>
+          <UtrechtFormFieldTextbox></UtrechtFormFieldTextbox>
+          <UtrechtFormFieldTextbox></UtrechtFormFieldTextbox>
+          <UtrechtFormFieldTextbox></UtrechtFormFieldTextbox>
+          <UtrechtButton appearance="primary-action-button">Versturen</UtrechtButton>
         </main>
       </UtrechtPageContent>
     </Layout>
